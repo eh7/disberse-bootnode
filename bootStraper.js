d851805370463df2df8565700ae4eec077179207
Gavin Francis <gav@eh7.co.uk>
2019-01-18 17:31:22 +0000

    updates latest

--- bootStraper.js
+++ bootStraper.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-console */
 'use strict'
 
+const contract = require('./contract')
+const lodash = require('lodash')
+
 const libp2p = require('libp2p')
 const TCP = require('libp2p-tcp')
 const WebSockets = require('libp2p-websockets')
 const WebRTCStar = require('libp2p-webrtc-star')
 const SPDY = require('libp2p-spdy')
+//const tls = require('libp2p-tls')
 const SECIO = require('libp2p-secio')
 const KadDHT = require('libp2p-kad-dht')
 const Protector = require('libp2p-pnet')
@@ -20,7 +24,9 @@ const defaultsDeep = require('@nodeutils/defaults-deep')
 const Circuit = require('libp2p-circuit')
 const multiaddr = require('multiaddr')
 const Catch = require('pull-catch')
+const ethWallet = require('ethereumjs-wallet')
 const ethUtil = require('ethereumjs-util')
+const ethTx = require('ethereumjs-tx')
 
 const directorId = require('./director-id.json')
 /*
@@ -81,9 +87,216 @@ let node
 let peerCheck = []
 let peerMap = []
 
+//ection getContractBalances(callback) {
+const getGasPriceAndNonce = () => {
+}
+
+const getContractBalances = (msg, callerAddress, callback) => {
+  let disberse_abi = contract.abi;
+  let owner_address = contract.owner_address;
+  let contract_address = contract.contract_address;
+  let Disberse = new web3.eth.Contract(disberse_abi, contract_address);
+
+  let results = []
+  let errors  = []
+  
+  for(let i=0; i<4; i++) {
+
+    let tokenId = i
+
+    Disberse.methods.getBalance(callerAddress,tokenId).call({from:callerAddress}, function(err,res) {
+      if(err){
+        errors.push(err)
+      } else {
+        var return_val = {id:tokenId,balance:Number(res).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}
+        results.push(return_val)
+
+        if(i == 3) {
+          if(errors.length > 0)
+            callback(errors)
+          else {
+
+            let sendToPeerId = PeerId.createFromB58String(msg.from)
+            console.log("id: " + sendToPeerId)
+            PeerInfo.create(sendToPeerId, (err, sendToPeerInfo) => {
+
+              node.dialProtocol(sendToPeerInfo, 
+                '/disberse/txBalances', 
+                (err, conn) => {
+                if(err) console.log(err)
+                else if(typeof results[3].balance === 'undefined') console.log("balance[3] error")
+                else {
+                  console.log("sending tx return")
+                  pull(
+//                    pull.values(["hello"]),
+                    pull.values([results[0].balance,
+                      results[1].balance,
+                      results[2].balance,
+                      results[3].balance]),
+                    conn
+                  )
+                  console.log("sent /disberse/txBalances")
+                  callback(results)
+                }              
+              })
+            })
+          }
+        }
+      }
+    })
+
+  }
+
+/*
+  PeerId.createFromPrivKey(directorId.privKey, (err, id) => {
+    const myPrivateKey = Buffer.from(id.toHexString().substr(4), 'hex')
+    const myPublicKey = ethUtil.privateToAddress(myPrivateKey).toString('hex')
+    const myAddress = ethUtil.privateToAddress(myPrivateKey).toString('hex')
+    const myWallet = ethWallet.fromPrivateKey(myPrivateKey)
+
+    web3.eth.getGasPrice(function(err, gasPrice) {
+      if(err)
+        console.log(err)
+    
+      let gasPriceHex = web3.utils.toHex(gasPrice)
+      let gasLimitHex = web3.utils.toHex(2000000)
+
+      let amount = 1
+      let typeId = 0
+
+      
+      //getBalance(address account_address, uint token_type_in)
+      Disberse.methods.getBalance(myAddress,typeId).estimateGas(function(error, gasAmount){
+        if(error)
+          console.log(error)
+        else {
+          callback("Gas estimnate for getBalance(" + myAddress + "," + typeId +") ")
+          web3.eth.getTransactionCount("0x"+myAddress, function(err,nonce) {
+            if(err)
+              console.log(err)
+
+            let nonceHex = web3.utils.toHex(nonce)
+            let transValue = web3.utils.toHex(0)
+
+            let lodash = require('lodash')
+            let this_function_abi = lodash.find(contract.abi, { name: 'getBalance' })
+            let payloadData = [myAddress,typeId]
+            let txPayloadData = web3.eth.abi.encodeFunctionCall(this_function_abi, payloadData)
+            let thisTx = {
+              from: "0x"+myAddress,
+              to: contract_address,
+              value: transValue,
+              data: txPayloadData,
+              nonce: nonceHex,
+              gasPrice: gasPriceHex,
+              gasLimit: gasLimitHex,
+            }
+            let signedTx = new ethTx(thisTx)
+            signedTx.sign(myPrivateKey)
+            let serializedTx = signedTx.serialize()
+            web3.eth.sendSignedTransaction("0x" + serializedTx.toString('hex'), function(err,hash){
+              if(err){
+                console.log(err);
+                callback("gp:"+gasPrice, myPublicKey)
+              }else {
+                console.log(hash)
+                callback("rawTx -> 0x" + serializedTx.toString('hex'))
+              }
+            })
+          })
+        }
+      })
+    })
+  })
+*/
+}
+
+const addDepositBalances = (callback) => {
+  let disberse_abi = contract.abi;
+  let owner_address = contract.owner_address;
+  let contract_address = contract.contract_address;
+  let Disberse = new web3.eth.Contract(disberse_abi, contract_address);
+
+  PeerId.createFromPrivKey(directorId.privKey, (err, id) => {
+    const myPrivateKey = Buffer.from(id.toHexString().substr(4), 'hex')
+    const myPublicKey = ethUtil.privateToAddress(myPrivateKey).toString('hex')
+    const myAddress = ethUtil.privateToAddress(myPrivateKey).toString('hex')
+
+    const myWallet = ethWallet.fromPrivateKey(myPrivateKey)
+/*
+    console.log(myWallet.getAddress())
+    console.log(myWallet.getV3Filename())
+    console.log(myWallet.toV3String('123'))
+*/
+//    console.log(myWallet)
+
+//    callback("myPrivateKey: " + myPrivateKey.toString('hex')) 
+
+    web3.eth.getGasPrice(function(err, gasPrice) {
+      if(err)
+        console.log(err)
+    
+      let gasPriceHex = web3.utils.toHex(gasPrice)
+      let gasLimitHex = web3.utils.toHex(2000000)
+
+      let amount = 1
+      let type_id = 1
+      let project_ref = "project ref"
+      
+       
+      Disberse.methods.deposit(amount,web3.utils.fromAscii(project_ref),type_id).estimateGas(function(error, gasAmount){
+        if(error)
+          console.log(error)
+        else {
+          web3.eth.getTransactionCount("0x"+myAddress, function(err,nonce) {
+            if(err)
+              console.log(err)
+
+            let nonceHex = web3.utils.toHex(nonce)
+            let transValue = web3.utils.toHex(0)
+
+            let lodash = require('lodash')
+            let this_function_abi = lodash.find(contract.abi, { name: 'deposit' })
+            let payloadData = [amount,web3.utils.fromAscii(project_ref),type_id]
+            let txPayloadData = web3.eth.abi.encodeFunctionCall(this_function_abi, payloadData)
+console.log(this_function_abi)
+            let thisTx = {
+              from: "0x"+myAddress,
+              to: contract_address,
+              value: transValue,
+              data: txPayloadData,
+              nonce: nonceHex,
+              gasPrice: gasPriceHex,
+              gasLimit: gasLimitHex,
+            }
+            let signedTx = new ethTx(thisTx)
+            signedTx.sign(myPrivateKey)
+            let serializedTx = signedTx.serialize()
+
+/*
+            web3.eth.sendSignedTransaction("0x" + serializedTx.toString('hex'), function(err,hash){
+              if(err){
+                console.log(err);
+                callback("gp:"+gasPrice, myPublicKey)
+              }else {
+*/
+                console.log("gasAmount: " + gasAmount)
+                console.log("nonce: " + nonce)
+                console.log("transValue: " + transValue)
+//                console.log(hash)
+                callback("gp:"+gasPrice, myPublicKey)
+//              }
+//            })
+          })
+        }
+      })
+    })
+  })
+}
+
 const listenForTxs = (node) => { 
   node.pubsub.subscribe('disberse/txs',(msg) => {
-    console.log(msg.from, msg.data.toString())
+    console.log("---------TXS-------------------\n", msg.from, msg.data.toString())
     let dataIn = msg.data.toString().split('::')
     let vrs = dataIn[dataIn.length-1].split(',')
     let data = dataIn[dataIn.length-2]
@@ -104,7 +317,21 @@ const listenForTxs = (node) => {
       console.log("Singnature is VALID")
       console.log("get balance web3 " + dataIn[3])
 
-      if(dataIn[2] == 'balance') {
+
+      if(dataIn[2] == 'newProject') {
+        console.log('newProject')
+      } else if(dataIn[2] == 'balanceForType') {
+        console.log('balanceForType')
+
+        let balances = []
+        let balanceAddr = dataIn[3]
+
+//        balances = getAllBalances()
+        getContractBalances(msg, balanceAddr, console.log)
+
+        console.log("Balances for " + balanceAddr + " are " + balances)
+
+      } else if(dataIn[2] == 'balance') {
         let balanceAddr = dataIn[3]
         web3.eth.getBalance(balanceAddr, function(err, balance) {
           if(err)
@@ -253,6 +480,85 @@ PeerInfo.create(directorId, (err, peerInfo) => {
     })
 */
 
+  })
+
+  node.handle('/disberse/sendtx', (protocol, conn) => {
+    console.log('handle /disberse/sendtx')
+    pull(
+      conn,
+      pull.asyncMap((data, callback) => {
+        web3.eth.sendSignedTransaction("0x" + data.toString(), function(err,hash){
+          if(err){
+            console.log("hash err: " + err);
+            callback(err,hash)
+          }else {
+            console.log("hash: " + hash)
+            callback(null,hash)
+          }
+        })
+      }),
+      conn
+    )
+  })
+
+  node.handle('/disberse/deposit', (protocol, conn) => {
+    console.log('handle /disberse/deposit')
+    pull(
+      conn,
+      pull.asyncMap((data, callback) => {
+        web3.eth.sendSignedTransaction("0x" + data.toString(), function(err,hash){
+          if(err){
+            console.log("hash err: " + err);
+            callback(err,hash)
+          }else {
+            console.log("hash: " + hash)
+            callback(null,hash)
+          }
+        })
+      }),
+      conn
+    )
+/*
+    pull(
+      conn,
+      pull.map((data) => {
+        console.log(data.toString())
+        web3.eth.sendSignedTransaction("0x" + data.toString(), function(err,hash){
+          if(err){
+            console.log(err);
+          }else {
+            console.log(hash)
+          }
+          console.log("hash: " + hash)
+        })
+//        web3.eth.sendSignedTransaction('0x' + data.toString(),(hash) => {
+//          console.log("hash: " + hash)
+//        })
+      }),
+      pull.drain(()=>{})
+    )
+*/
+  })
+
+  node.handle('/disberse/gasnnonce', (protocol, conn) => {
+    console.log("/disberse/gasnnonce")
+    pull(
+      conn,
+/*
+      pull.map((data) => {
+        return data.toString()
+//        return data.toString().toUpperCase() + '!!!'
+      }),
+*/
+      pull.asyncMap((data, callback) => {
+        web3.eth.getGasPrice(function(err, gasPrice) {
+          web3.eth.getTransactionCount(data.toString(), function(err,nonce) {
+            callback(null,data.toString()+"::"+gasPrice+"::"+nonce)
+          })
+        })
+      }),
+      conn
+    )
   })
 
   node.handle('/message', (protocol, conn) => {
